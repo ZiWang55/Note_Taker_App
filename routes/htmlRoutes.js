6a9a9c5030129ed14adb03ec619f0bcb8cd6e229
zi wang <ziwang55@gmail.com>
2020-12-12 22:39:18 -0600

    router works and gitignore edited

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,7 +4,7 @@ var path = require("path");
 // Routing
 module.exports = function(app) {
     // HTML get requests to direct them into the HTML page
-    app.get("*", function(req, res) {
+    app.get("/", function(req, res) {
         res.sendFile(path.join(__dirname, "../Develop/public/index.html"))
     });
     app.get("/notes", function(req, res) {
