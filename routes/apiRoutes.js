9eed9f4ee80794146e07e9e3cd8cc01cbfa1846f
zi wang <ziwang55@gmail.com>
2020-12-12 23:57:25 -0600

    adding routing for api

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,7 +2,9 @@
 const savedNotesData = require("../Develop/db/db.json");
 //Routing
 module.exports = function(app) {
-    
+    app.get("/api/notes", function (req, res) {
+        res.json(db.json);
+    });
 }
 //Api post requests
 
