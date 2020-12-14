48312b319f0e354d1d37ae5cb3a1b2d76943fd7c
zi wang <ziwang55@gmail.com>
2020-12-13 23:03:01 -0600

    fixed error with apiRoutes.js, tested get post and del. works

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,6 +6,7 @@ var data = JSON.parse(fs.readFileSync("./Develop/db/db.json", "utf8"));
 // const savedNotesData = require("../Develop/db/db.json");
 //Routing
 module.exports = function (app) {
+    //Api get
   app.get("/api/notes", function (req, res) {
     res.json(data);
   });
@@ -16,21 +17,30 @@ module.exports = function (app) {
 
   //Api post requests
   app.post("/api/notes", function (req, res) {
-    data.push(req.body);
+    let newNote = req.body;
+    let uniqueId = (data.length).toString();
+    // console.log(uniqueId);
+    newNote.id = uniqueId;
+    data.push(newNote);
+
+    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(data), function(error) {
+        if (error) throw (error);
+    } );
+    res.json(data);
   });
 
-//   Delete 
-app.delete("/api/notes/:id", function(req, res) {
+  //Api delete
+  app.delete("/api/notes/:id", function (req, res) {
     let noteId = req.params.id;
     let newId = 0;
-    data = data.filter(currentNote => {
-        return currentNote.id != noteId;
+    data = data.filter((currentNote) => {
+      return currentNote.id != noteId;
     });
     for (currentNote of data) {
-        currentNote.id = newId.toString();
-        newId++;
+      currentNote.id = newId.toString();
+      newId++;
     }
-    fs.writeFileSync("../Develop/db/db.json", JSON.stringify(data));
+    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(data));
     res.json(data);
-});
+  });
 };
